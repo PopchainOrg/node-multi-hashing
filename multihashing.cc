8e5e80ac313058522853a1d8cc93747dd7978ae8
crazyfrog2001 <lqddx@163.com>
2018-10-30 14:47:08 +0800

    add initOneWayFunction

--- multihashing.cc
+++ multihashing.cc
@@ -34,7 +34,7 @@ NAN_METHOD(cryptopop) {
 
     uint32_t input_len = Buffer::Length(target);
     //input_len=input_len;
-    
+    initOneWayFunction();
     hashpop(input, input_len, output);
 
     v8::Local<v8::Value> returnValue = Nan::CopyBuffer((char *)output, OUTPUT_LEN).ToLocalChecked();
